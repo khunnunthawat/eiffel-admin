6c1cbc6c3e17798f299c53e5d29d44738be93215
khunnunthawat <khunnunthawat@gmail.com>
2021-07-09 02:34:25 +0700

    header-v.0.1-test

---
+++ src/components/Layout/Sider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sider: React.FC = () => {
+  return (
+    <div>
+      <h1>Sider</h1>
+    </div>
+  );
+};
+
+export default Sider;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head';
+import Header from '@/components/Layout/Header';
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div className='min-h-screen'>
       <Head>
         <title>Eiffel Admin</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Header />
       <div className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center text-blue-500'>
-        <p className='mt-3 text-2xl'>Eiffel Admin</p>
+        <p className='mt-3 text-base'>Eiffel Admin</p>
       </div>
     </div>
   );
